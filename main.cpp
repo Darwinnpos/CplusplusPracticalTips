8c54b0f51e63240cd81b01da088f2609434a6c2e
smarttrandyo <smarttrandyo@hotmail.com>
2024-06-20 19:10:24 +0800

    提交修改

--- main.cpp
+++ main.cpp
@@ -12,12 +12,18 @@
 #include <optional>
 #include <functional>
 
+
+// 参数包装器
 template<typename T>
 struct input { T& t; };
 
 template<typename T>
 struct output { T& t; };
 
+template<typename T>
+struct unused { T& t; };
+
+// wrapper 概念
 template<template<typename> class U, typename T>
 struct is_instance_of : std::false_type {};
 
@@ -25,8 +31,9 @@ template<template<typename> class U, typename T>
 struct is_instance_of<U, U<T>> : std::true_type {};
 
 template<typename T>
-concept wrapper = is_instance_of<input, T>::value || is_instance_of<output, T>::value;
+concept wrapper = is_instance_of<input, T>::value || is_instance_of<output, T>::value || is_instance_of<unused, T>::value;
 
+// 包参数处理 
 template<typename Tuple>
 struct tuples;
 
@@ -42,7 +49,7 @@ auto inputs(_Types&&... _Args) { return std::make_tuple(input(_Args)...); }
 template<typename T>
 void print_element(const T& t)
 {
-	//std::cout << typeid(T).name() << ": " << t;
+	std::cout << typeid(T).name() << ": " << t;
 }
 
 template<typename T>
@@ -112,40 +119,75 @@ protected:
 
 class semaphore
 {
-
-
+public:
+	void release() { }
+	void wait() { }
 };
 
 template<typename T>
 class mq
 {
-
-
+public:
+	T pop()
+	{
+		return T();
+	}
+	void push(T&)
+	{
+	}
 };
 
-class event
+enum class terminal
+{
+	client,
+	server
+};
+
+template<terminal Terminal>
+class event_list
 {
 public:
-	void allocte() {}
+	size_t allocte() {
+		thread_local size_t index{ index_mq.pop() };
+		return index;
+	}
 
 private:
 	std::vector<semaphore> semaphore_array;
-	mq<size_t> index;
+	mq<size_t> index_mq;
 };
 
 
-class tls_memory : public malloc_chain
+class basic_shared_memory
+{
+public:
+	char* get() { return addr; }
+
+private:
+	size_t size;
+	char* addr;
+};
+
+template<terminal Terminal>
+class tls_memory : public malloc_chain 
 {
 public:
 	std::optional<char*> malloc_inner(size_t size) override {
-		return {};
+		auto index = synchronize.allocte();
+		if (size > capacity) return {};
+		return memory_list.at(size).get();
 	}
+private:
+	const size_t capacity{ 4096 };
+	std::vector<basic_shared_memory> memory_list;
+	event_list<Terminal> synchronize;
 };
 
 class managered_memory : public malloc_chain
 {
 public:
 	std::optional<char*> malloc_inner(size_t size)override {
+		// TODO 		
 		return {};
 	}
 };
@@ -154,21 +196,23 @@ class file_transefer : public malloc_chain
 {
 public:
 	std::optional<char*> malloc_inner(size_t size) override {
+		// TODO 		
 		return {};
 	}
 };
 
+template<terminal Terminal>
 class shared_memory
 {
 public:
 	shared_memory()
 		:entrance{ first }
-	{		
+	{
 		first.set_next(second).set_next(last);
 	}
 
 	char* malloc(size_t size) {
-		synchronization.allocte();
+		
 		auto opt_prt = entrance.malloc(size);
 		if (opt_prt.has_value())return opt_prt.value();
 		throw std::runtime_error("shared memory malloc failed.");
@@ -183,9 +227,8 @@ public:
 		return *instance;
 	}
 private:
-	inline static shared_memory* instance = nullptr;
-	event synchronization;
-	tls_memory first;
+	inline static shared_memory* instance = nullptr;	
+	tls_memory<Terminal> first;
 	managered_memory second;
 	file_transefer last;
 	malloc_chain& entrance;
@@ -193,112 +236,110 @@ private:
 
 class seriliasize
 {
+public:
 	template<typename... Ts>
 	static void to_parameters(std::tuple<Ts...>& theTuple)
 	{
-		auto [ptr, size] = shared_memory::get().find();
-		yas::load(ptr, size, theTuple);
+		auto [ptr, size] = shared_memory<terminal::client>::get().find();
+		yas::load(ptr, size, 0);
 
 		std::cout << "deserialize:";
+		
 		std::apply([](const Ts&... args) {
 			std::size_t n = 0;
-			((std::cout << (n++ ? ", " : "") << typeid(args).name() << ": ", print_element(args)), ...);
+			((std::cout << (n++ ? ", " : "") << typeid(args.t).name() << ": ", print_element(args)), ...);
 			}, theTuple);
+
 		std::cout << std::endl;
 	}
 
 
 	template<typename... Ts>
-	static size_t count(std::tuple<Ts...>& theTuple)
+	static size_t count(const std::tuple<Ts...>& theTuple)
 	{
 		return 0;
 	}
 
 	template<typename... Ts>
-	static void to_string(std::tuple<Ts...>& theTuple)
+	static void to_string(const std::tuple<Ts...>& theTuple)
 	{
 		// try malloc the shared memory	
-		auto ptr = shared_memory::get().malloc(count(theTuple));
-		yas::save(ptr, theTuple);
+		auto ptr = shared_memory<terminal::client>::get().malloc(count(theTuple));
+		yas::save(ptr, 0);
 		std::cout << "serialize:";
 		std::apply([](const Ts&... args) {
 			std::size_t n = 0;
-			((std::cout << (n++ ? ", " : "") << typeid(args).name() << ": ", print_element(args)), ...);
+			((std::cout << (n++ ? ", " : "") << typeid(args.t).name() << ": ", print_element(args)), ...);
 			}, theTuple);
 		std::cout << std::endl;
 	}
 };
 
 
-
+struct params;
 // 参数执行器
-class tls
+struct execute
 {
-	using execute = std::function<void()>;
-#define VOID_FN []{}
-public:
+	using fn = std::function<void()>;
+	fn write;
+	fn read;
+
 	template<typename... Args>
-	static void parameters_bind(Args&&... args)
+	void params_bind(Args&&... args)
 	{
-		auto [inputs, outputs] = parameters_sort(std::forward<Args>(args)...);
-		writter = [=]() mutable { seriliasize::to_string(inputs); };
-		reader = [=]() mutable { seriliasize::to_parameters(outputs); };
+		auto [inputs, outputs] = params::sort(std::forward<Args>(args)...);
+		write = [=]() { seriliasize::to_string(inputs); };
+		read = [=]() { seriliasize::to_parameters(outputs); };
 	}
-
-	static void wait_response()
+	void wait_response()
 	{
 
 	}
-
-	static void write()
-	{
-		writter();
-	}
-
-	static void read()
-	{
-		reader();
-	}
-
-private:
-	inline static thread_local execute writter = VOID_FN;
-	inline static thread_local execute reader = VOID_FN;
 };
 
 
-auto parameters_sort()
-{
-	return std::tuple<std::tuple<>, std::tuple<>>();
-}
-
-// 参数收集器
-template<typename First, typename... Rest>
-auto parameters_sort(First&& first, Rest&&... rest)
-{
-	auto rest_result = parameters_sort(std::forward<Rest>(rest)...);
-
-	if constexpr (std::is_same_v<std::decay_t<First>, input<std::decay_t<decltype(first.t)>>>)
+struct params {	
+	
+	static auto sort()
 	{
-		return std::tuple_cat(
-			std::make_tuple(std::tuple_cat(std::tuple<std::decay_t<First>>(std::forward<First>(first)), std::get<0>(rest_result))),
-			std::make_tuple(std::get<1>(rest_result))
-		);
+		return std::tuple<std::tuple<>, std::tuple<>>();
 	}
-	else {
-		return std::tuple_cat(
-			std::make_tuple(std::get<0>(rest_result)),
-			std::make_tuple(std::tuple_cat(std::tuple<std::decay_t<First>>(std::forward<First>(first)), std::get<1>(rest_result)))
-		);
+
+	// 参数收集器
+	template<wrapper First, wrapper... Rest>
+	static auto sort(First&& first, Rest&&... rest)
+	{
+		auto rest_result = sort(std::forward<Rest>(rest)...);
+
+		if constexpr (std::is_same_v<std::decay_t<First>, input<std::decay_t<decltype(first.t)>>>)
+		{
+			return std::tuple_cat(
+				std::make_tuple(
+					std::tuple_cat(std::tuple<std::decay_t<First>>(std::forward<First>(first)),
+						std::get<0>(rest_result))),
+				std::make_tuple(std::get<1>(rest_result))
+			);
+		}
+		else {
+			return std::tuple_cat(
+				std::make_tuple(std::get<0>(rest_result)),
+				std::make_tuple(
+					std::tuple_cat(std::tuple<std::decay_t<First>>(std::forward<First>(first)),
+						std::get<1>(rest_result)))
+			);
+		}
 	}
-}
+};
+
 
 template<wrapper... Args>
 void rpc(Args&&... args)
 {
-	tls::parameters_bind(std::forward<Args>(args)...);
-	tls::write();
-	tls::wait_response();
-	tls::read();
+	thread_local execute exe;
+	exe.params_bind(std::forward<Args>(args)...);
+	exe.write(); // 参数的分类调用
+	exe.wait_response();
+	exe.read();  // 参数的分类延迟调用
 }
 
 template<tuple_wrapper... Args>
@@ -317,9 +358,13 @@ int main()
 	double b{ 2 };
 	float c{ 3 };
 	bool d{ false };
+	int e{ 4 };
+	int f{ 6 };
 
-	rpc(input(a), input(b), output(c), output(d));
-
+	// 参数包装器应用实例
+	rpc(input(a), input(b), output(c), output(d), output(e), input(f)); //9 * 400  3600个手动编写的代码，或者400个耦合复杂逻辑的代码
+	
+	rpc(input(a), input(b), output(c), output(d), output(e), input(f)); 
 	//rpc(inputs(a, b), output(c), output(d));
 
 	return 0;
